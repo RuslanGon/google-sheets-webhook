061ed1365e74047ad7101d53738948cf90da6cfb
RuslanGon <ruslan441006@gmail.com>
2024-10-15 11:44:32 +0300

    wer

--- src/config/db.js
+++ src/config/db.js
@@ -9,7 +9,6 @@ const connectDB = async () => {
     const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
 
     await mongoose.connect(uri, {
-      useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log('MongoDB connected successfully');
