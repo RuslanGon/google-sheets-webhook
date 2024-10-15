9e7fd837ce41225b54d67fa06fa5b00a60f69f46
RuslanGon <ruslan441006@gmail.com>
2024-10-15 11:47:42 +0300

    wer

--- src/config/db.js
+++ src/config/db.js
@@ -8,9 +8,7 @@ const connectDB = async () => {
   try {
     const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
 
-    await mongoose.connect(uri, {
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(uri);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
